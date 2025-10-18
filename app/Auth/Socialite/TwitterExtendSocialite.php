56b41ec6fe3bbbff1b3febe8182f472fb0fa3d86
giveablu <giveablu@gmail.com>
2025-10-18 13:50:00 -0400

    Update TwitterExtendSocialite.php

--- app/Auth/Socialite/TwitterExtendSocialite.php
+++ app/Auth/Socialite/TwitterExtendSocialite.php
@@ -3,11 +3,12 @@
 namespace App\Auth\Socialite;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Twitter\Server as BaseTwitterServer;
 
 class TwitterExtendSocialite
 {
     public function handle(SocialiteWasCalled $event): void
     {
-        $event->extendSocialite('twitter', TwitterProvider::class, 'SocialiteProviders\\Twitter\\Server');
+        $event->extendSocialite('twitter', TwitterProvider::class, BaseTwitterServer::class);
     }
 }
